b03550fe5e6524357555e5762f8467ef9be5f881
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2026-08-05 20:07:28 +0530

    feat: implement root layout with SEO metadata, custom fonts, and third-party tracking scripts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -117,7 +117,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '26894717766863044');
+fbq('init', '1712181646718693');
 fbq('track', 'PageView');`
           }}
         />
@@ -172,7 +172,7 @@ fbq('track', 'PageView');`
             height="1"
             width="1"
             style={{ display: 'none' }}
-            src="https://www.facebook.com/tr?id=26894717766863044&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1712181646718693&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
